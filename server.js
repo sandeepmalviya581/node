a5cf0eb3863f7f97fe10b30f34b66722fb025529
Sachin Choudhary <sachin.choudhary@divergentsl.com>
2018-09-04 13:49:24 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,12 @@
+var http = require('http');
+
+http.createServer((req, res) => {
+
+    res.writeHead(200, { 'Content-Type': 'text/html' });
+    res.write("hello ram.");
+    res.write(req.url);
+
+
+    
+    res.end();
+}).listen(8000);
